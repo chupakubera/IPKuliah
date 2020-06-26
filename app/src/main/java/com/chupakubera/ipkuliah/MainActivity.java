9bf059671f70622cf4110310f93625da2bdabc0b
chupakubera <allanggaongi@gmail.com>
2020-06-26 17:48:03 +0700

    Membuat design logo dan tampilan loading screen aplikasi.

--- app/src/main/java/com/chupakubera/ipkuliah/MainActivity.java
+++ app/src/main/java/com/chupakubera/ipkuliah/MainActivity.java
@@ -1,14 +1,31 @@
 package com.chupakubera.ipkuliah;
 
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+    private int loading_time = 4000;    // 4 detik waktu loading screen
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                // loading screen 4 detik lalu pindah ke home
+                Intent home=new Intent(MainActivity.this, HomeActivity.class);
+                startActivity(home);
+                finish();
+
+            }
+        },loading_time);
     }
 }
